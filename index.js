3f557e836d300e0dd9d6427a1d2674854b9f6ab2
David Salazar <david.asal@hotmail.com>
2018-05-31 14:53:00 -0500

    Finanlly added no mutual fund page and updated links

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 window.addEventListener("scroll", function () {
     console.log(window.pageYOffset);
-    if (window.pageYOffset > 300) {
+    if (window.pageYOffset > 200) {
         $('.nav').fadeIn('slow');
         $('.nav').css('top', window.pageYOffset);
     } else {
@@ -13,10 +13,16 @@ window.addEventListener("scroll", function () {
 
 window.addEventListener("scroll", function () {
     console.log(window.pageYOffset);
-    if (window.pageYOffset > 300) {
+    if (window.pageYOffset > 200) {
         $('.navs').fadeIn('slow');
         $('.navs').css('top', window.pageYOffset);
     } else {
         $('.navs').fadeOut();
     }
 }, false);
+
+$(function () {
+  $('[data-toggle="popover"]').popover();
+});
+
+$('#element').popover('toggle');
